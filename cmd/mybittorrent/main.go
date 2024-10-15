d1d0595fdaea0c172735c2432296f3acaa09a651
Lucas <lucasbrodrigues14@gmail.com>
2024-10-15 02:07:32 +0000

    adds parsing for torrent files

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/jackpal/bencode-go"
 	"os"
 	"strings"
-
-	"github.com/jackpal/bencode-go"
 )
 
 // Example:
@@ -21,10 +20,54 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 	}
 	return data, nil
 }
+func parseTorrentFile(location string) (string, int64, error) {
+	data, err := os.ReadFile(location)
+	var tracker string
+	var length int64
+	if err != nil {
+		return "", 0, err
+	}
+	decodedData, err := decodeBencode(string(data))
+	if resultMap, ok := decodedData.(map[string]interface{}); ok {
+		if announce, ok := resultMap["announce"].(string); ok {
+			tracker = announce
+		}
+	}
+
+	if resultMap, ok := decodedData.(map[string]interface{}); ok {
+		if info, ok := resultMap["info"].(map[string]interface{}); ok {
+			if l, ok := info["length"].(int64); ok {
+				length = l
+			}
+		}
+	}
+	if err != nil {
+		return "", 0, err
+	}
+	return tracker, length, nil
+
+	//if unicode.IsDigit(rune(bencodedString[0])) {
+	//	var firstColonIndex int
+
+	//	for i := 0; i < len(bencodedString); i++ {
+	//		if bencodedString[i] == ':' {
+	//			firstColonIndex = i
+	//			break
+	//		}
+	//	}
+
+	//	lengthStr := bencodedString[:firstColonIndex]
+
+	//	length, err := strconv.Atoi(lengthStr)
+	//	if err != nil {
+	//		return "", err
+	//	}
+
+	//	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
+	//}
+}
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Println("Logs from your program will appear here!")
 
 	command := os.Args[1]
 
@@ -40,6 +83,15 @@ func main() {
 
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
+	} else if command == "info" {
+		filePath := os.Args[2]
+		tracker, length, err := parseTorrentFile(filePath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Tracker URL:", tracker)
+		fmt.Println("Length:", length)
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
