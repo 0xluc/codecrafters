8ba7f22db6db73889b4e9a5fbb22a7ba081ba3cb
Lucas <lucasbrodrigues14@gmail.com>
2024-10-19 00:20:18 +0000

    I will retry that challenge in another day, rn I'm a bit stuck in problem 9

--- bittorrent/cmd/mybittorrent/main.go
+++ bittorrent/cmd/mybittorrent/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bytes"
 	"crypto/sha1"
+	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
+	"math"
 	"net"
 	"strconv"
 	"time"
@@ -145,6 +148,37 @@ func main() {
 			return
 		}
 		fmt.Println("Peer ID:", peerHandshake(parsedTorrent, os.Args[3]))
+	case "download_piece":
+		var torrentFile, outputPath string
+		if os.Args[2] == "-o" {
+			torrentFile = os.Args[4]
+			outputPath = os.Args[3]
+		} else {
+			torrentFile = os.Args[4]
+			outputPath = "."
+		}
+		parsedTorrent, err := parseTorrentFile(torrentFile)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		ind, _ := strconv.Atoi(os.Args[5])
+		peers, err := getPeers(parsedTorrent)
+		if err != nil {
+			fmt.Println(err)
+		}
+		data := downloadFile(parsedTorrent, peers[0], ind)
+
+		file, err := os.Create(outputPath)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer file.Close()
+		_, err = file.Write(data)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("Piece downloaded to %s.\n", outputPath)
 	default:
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
@@ -235,4 +269,129 @@ func peerHandshake(parsedTorrent ParsedTorrent, address string) string {
 	hexRepresentation := hex.EncodeToString(buffer[len(buffer)-20:])
 
 	return hexRepresentation
+}
+
+type PeerMessage struct {
+	lengthPrefix uint32
+	id           uint8
+	index        uint32
+	begin        uint32
+	length       uint32
+}
+
+func downloadFile(parsedTorrent ParsedTorrent, address string, index int) []byte {
+	infoHash, err := hexToASCII(parsedTorrent.InfoHash)
+	protocolLength := byte(19)
+	concatenatedBytes := append([]byte{protocolLength}, []byte("BitTorrent protocol")...)
+	concatenatedBytes = append(concatenatedBytes, make([]byte, 8)...)
+	concatenatedBytes = append(concatenatedBytes, []byte(infoHash)...)
+	concatenatedBytes = append(concatenatedBytes, []byte("00112233445566778889")...)
+
+	conn, err := net.DialTimeout("tcp", address, 5*time.Second)
+	if err != nil {
+		fmt.Println("Error connecting to server,", err)
+	}
+	defer conn.Close()
+	_, err = conn.Write(concatenatedBytes)
+	if err != nil {
+		fmt.Println("Error sending concatenated bytes:", err)
+	}
+
+	buffer := make([]byte, 1024)
+	_, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Println("error reading", err)
+	}
+
+	// wait for the bitfield message id=5
+	buf := make([]byte, 4)
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+	}
+	peerMessage := PeerMessage{}
+	peerMessage.lengthPrefix = binary.BigEndian.Uint32(buf)
+	payloadBuf := make([]byte, peerMessage.lengthPrefix)
+	_, err = conn.Read(payloadBuf)
+	if err != nil {
+		fmt.Println(err)
+	}
+	peerMessage.id = payloadBuf[0]
+
+	fmt.Printf("Received message: %v\n", peerMessage.id)
+	if peerMessage.id != 5 {
+		fmt.Println("Bitfield message not found")
+	}
+
+	// send 2
+	_, err = conn.Write([]byte{0, 0, 0, 1, 2})
+	if err != nil {
+		fmt.Println(err)
+	}
+	//wait for 1
+	buf = make([]byte, 4)
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+	}
+	peerMessage = PeerMessage{}
+	peerMessage.lengthPrefix = binary.BigEndian.Uint32(buf)
+	payloadBuf = make([]byte, peerMessage.lengthPrefix)
+	_, err = conn.Read(payloadBuf)
+	if err != nil {
+		fmt.Println(buf)
+	}
+	peerMessage.id = payloadBuf[0]
+	fmt.Printf("Received message: %v\n", peerMessage.id)
+	if peerMessage.id != 1 {
+		fmt.Println(buf)
+		fmt.Println("Unckoke message not found")
+	}
+
+	// request 6
+	pieceSize := parsedTorrent.PieceLength
+	pieceCnt := int(math.Ceil(float64(parsedTorrent.Length) / float64(pieceSize)))
+	if index == pieceCnt-1 {
+		pieceSize = parsedTorrent.Length % parsedTorrent.PieceLength
+	}
+	blockSize := 16 * 1024
+	blockCnt := int(math.Ceil(float64(pieceSize) / float64(blockSize)))
+	fmt.Printf("File Length: %d, Piece Length: %d, Piece Count: %d, Block Size: %d, Block Count: %d\n", parsedTorrent.Length, parsedTorrent.PieceLength, pieceCnt, blockSize, blockCnt)
+	var data []byte
+	for i := 0; i < blockCnt; i++ {
+		blockLength := blockSize
+		if i == blockCnt-1 {
+			blockLength = int(pieceSize) - ((blockCnt - 1) * int(blockSize))
+		}
+		peerMessage := PeerMessage{
+			lengthPrefix: 13,
+			id:           6,
+			index:        uint32(index),
+			begin:        uint32(i * int(blockSize)),
+			length:       uint32(blockLength),
+		}
+		var buf bytes.Buffer
+		binary.Write(&buf, binary.BigEndian, peerMessage)
+		_, err = conn.Write(buf.Bytes())
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Sent request message", peerMessage.id)
+		resBuf := make([]byte, 4)
+		_, err = conn.Read(resBuf)
+		if err != nil {
+			fmt.Println(err)
+		}
+		peerMessage = PeerMessage{}
+		peerMessage.lengthPrefix = binary.BigEndian.Uint32(resBuf)
+		payloadBuf := make([]byte, peerMessage.lengthPrefix)
+		_, err = io.ReadFull(conn, payloadBuf)
+		if err != nil {
+			fmt.Println(err)
+		}
+		peerMessage.id = payloadBuf[0]
+		fmt.Printf("Received message: %v\n", peerMessage.id)
+		data = append(data, payloadBuf[9:]...)
+	}
+	return data
 }
